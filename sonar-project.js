ca35273a55db5e6c4b92ddefbacdee30f7b4a7e9
dkokmadis <dkokmadis@gmail.com>
2020-12-09 19:50:58 +0200

    Create  sonar-project.js

---
+++ sonar-project.js
@@ -0,0 +1,13 @@
+const sonarqubeScanner =  require('sonarqube-scanner');
+sonarqubeScanner(
+    {
+        serverUrl:  'http://18.197.213.195:9003',
+        options : {
+            'sonar.sources':  'src',
+            'sonar.tests':  'src',
+            'sonar.inclusions'  :  '**', // Entry point of your code
+            'sonar.test.inclusions':  'src/**/*.spec.js,src/**/*.spec.jsx,src/**/*.test.js,src/**/*.test.jsx',
+            'sonar.javascript.lcov.reportPaths':  'coverage/lcov.info',
+            'sonar.testExecutionReportPaths':  'coverage/test-reporter.xml'
+        }
+    }, () => {});
